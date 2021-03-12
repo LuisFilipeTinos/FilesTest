9d0dab68617ab352c46281b14c80c2d9661fcda7
Luis Filipe Tinos <lftinos@gmail.com>
2021-03-12 18:10:49 -0300

    First commit

---
+++ ProblemaLocação/Entities/Location.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ProblemaLocação
+{
+    class Location
+    {
+        public string CarModel { get; private set; }
+        public DateTime InitialDate { get; private set; }
+        public DateTime FinalDate { get; private set; }
+        public double HourValue { get; private set; }
+        public double DaylyValue { get; private set; }
+        public TimeSpan DurationTime { get; set; }
+        public double TaxesValue { get; private set; }
+
+        public Location(string carModel, DateTime initialDate, DateTime finalDate, double hourValue, double daylyValue)
+        {
+            CarModel = carModel;
+            InitialDate = initialDate;
+            FinalDate = finalDate;
+            HourValue = hourValue;
+            DaylyValue = daylyValue;
+        }
+
+        public TimeSpan Duration()
+        {
+            DurationTime = FinalDate.Subtract(InitialDate);
+            return DurationTime;
+        }
+
+        public double Values()
+        {
+            
+ 
+            if (DurationTime.Hours < 12 && DurationTime.Days < 1)
+            {
+                return HourValue * Math.Ceiling(DurationTime.TotalHours);
+            }
+            else
+            {
+                return DaylyValue * Math.Ceiling(DurationTime.TotalHours);
+            }
+        }
+
+        public double ValueWithTaxes()
+        {
+            if (Values() < 100.00)
+            {
+                TaxesValue = 0.2 * Values();
+                return Values() + TaxesValue;
+            }
+            else if (Values() > 100.00)
+            {
+                TaxesValue = 0.15 * Values();
+                return Values() + TaxesValue;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("RENT INFO:");
+            sb.AppendLine("Car model: " + CarModel);
+            sb.AppendLine("Rent duration: " + DurationTime);
+            sb.AppendLine("Total payied: " + ValueWithTaxes().ToString("F2", CultureInfo.InvariantCulture));
+            sb.AppendLine("");
+            sb.AppendLine("Thanks for the preference!");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ProblemaLocação/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+
+namespace ProblemaLocação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            try
+            {
+                Console.WriteLine("Enter rental data");
+                Console.Write("Car model: ");
+                string carModel = Console.ReadLine();
+                Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+                DateTime initialDate = DateTime.Parse(Console.ReadLine());
+                Console.Write("Return (dd/MM/yyyy hh:mm): ");
+                DateTime finalDate = DateTime.Parse(Console.ReadLine());
+                Console.Write("Enter price per hour: ");
+                double hourValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Enter price per day: ");
+                double daylyValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Location location = new Location(carModel, initialDate, finalDate, hourValue, daylyValue);
+
+                location.Values();
+                location.Duration();
+                location.ValueWithTaxes();
+
+
+                Directory.CreateDirectory(path + @"\Out");
+                string filePath = Path.Combine(path + @"\Out\rent.txt");
+
+                Console.WriteLine();
+                Console.WriteLine("INVOICE");
+                Console.WriteLine("Basic payment: " + location.Values().ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Tax: " + location.TaxesValue.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Total payment: " + location.ValueWithTaxes().ToString("F2", CultureInfo.InvariantCulture));
+
+                using (StreamWriter sw = File.AppendText(filePath))
+                {
+                    sw.WriteLine(location);
+                }
+
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured!");
+                Console.WriteLine(e.Message);
+            }
+            
+
+        }
+    }
+}
